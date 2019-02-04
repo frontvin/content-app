5b54827e02d49c0f020aa3ccce50c75e6aac4934
bvsbvs <bvs0618@gmail.com>
2019-02-04 14:25:24 +0200

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,134 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
+// App interfaces
+// button
+export interface IButton {
+  label: string;
+  activeButton?: boolean;
+  getContent?: () => void;
+}
+
+// content
+// export interface IContent {
+//   display: boolean;
+//   displayContent: () => string; // content displays as string
+// }
+
+// Content interfaces
+// posts
+interface IPosts {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+// comments
+interface IComments {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+// albums
+interface IAlbum {
+  userId: number;
+  id: number;
+  title: string;
+}
+
+// photos
+interface IPhotos {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+// todos
+interface ITodos {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+// users
+interface IUsers {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+// functional components
+
+const Button: React.FC<IButton> = ({label}: IButton) => {
+  return (
+      <button className="btn">
+        {label}
+      </button>)
+};
+
+const Content: React.FC = () => {
+  return (
+      <div className="content">
+
+      </div>
+  )
+}
+
+// App class
 class App extends Component {
+  // state
+  state = {
+    buttons: [
+      {label: "Posts", activeButton: false, getContent: () => {}},
+      {label: "Comments", activeButton: false, getContent: () => {}},
+      {label: "Albums", activeButton: false, getContent: () => {}},
+      {label: "Photos", activeButton: false, getContent: () => {}},
+      {label: "Todos", activeButton: false, getContent: () => {}},
+      {label: "Users", activeButton: false, getContent: () => {}}
+    ]
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="buttons">
+          {this.state.buttons.map((button, index) => {
+            return (
+                <Button
+                    key={index}
+                    label={button.label}
+                />
+              )
+            })
+          }
+        </div>
+
+        <Content />
       </div>
     );
   }
