10f30cf28f132eea8a2e419c58c277d6823640ce
bvsbvs <bvs0618@gmail.com>
2019-02-06 16:37:53 +0200

    first working version

--- src/App.tsx
+++ src/App.tsx
@@ -4,86 +4,83 @@ import axios from "axios";
 
 // App interfaces
 // button
-export interface IButton {
+interface IButton {
   label: string;
   activeButton?: boolean;
-  getContent?: () => void;
+  getContent?: () => void | string;
 }
 
 // content
-export interface IContent {
-  content: []
+interface IContent {
+  content: string
 }
 
-// Content interfaces
-// posts
 interface IPosts {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
 }
 
 // comments
 interface IComments {
-  postId: number;
-  id: number;
-  name: string;
-  email: string;
-  body: string;
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
 }
 
 // albums
 interface IAlbum {
-  userId: number;
-  id: number;
-  title: string;
+    userId: number;
+    id: number;
+    title: string;
 }
 
 // photos
 interface IPhotos {
-  albumId: number;
-  id: number;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
 }
 
 // todos
 interface ITodos {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
 }
 
 // users
 interface IUsers {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
+    id: number;
     name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
 }
 
 // functional components
-
 const Button: React.FC<IButton> = ({ label, getContent }: IButton) => {
   return (
       <button className="btn" onClick={getContent}>
@@ -95,51 +92,49 @@ const Button: React.FC<IButton> = ({ label, getContent }: IButton) => {
 const Content: React.FC<IContent> = ({ content }) => {
   return (
       <div className="content">
-        {content.map(item => (
-            <div key={item.id}>
-              <h4>{item.title}</h4>
-              <p>{item.body}</p>
-            </div>
-        ))}
+        {content}
       </div>
   );
 };
 
-
-
 // App class
+// add generic props, state
 class App extends Component {
-  // state
-  state = {
-    buttons: [
-      { label: "Posts", activeButton: false, getContent: () => {} },
-      { label: "Comments", activeButton: false, getContent: () => {} },
-      { label: "Albums", activeButton: false, getContent: () => {} },
-      { label: "Photos", activeButton: false, getContent: () => {} },
-      { label: "Todos", activeButton: false, getContent: () => {} },
-      { label: "Users", activeButton: false, getContent: () => {} }
-    ],
-    content: []
-  };
 
-  getContent = () => {
+      // state
+      state = {
+          content: ""
+      };
+
+
+
+  private getContent = () => {
     axios.get(`https://jsonplaceholder.typicode.com/posts`).then(res => {
-      const content: string = res.data;
-      this.setState({ content });
+      const content: string = JSON.stringify(res.data);
+      this.setState({content});
     });
   };
 
   render() {
+    const buttons = [
+       { label: "Posts", activeButton: false },
+       { label: "Comments", activeButton: false },
+       { label: "Albums", activeButton: false },
+       { label: "Photos", activeButton: false },
+       { label: "Todos", activeButton: false },
+       { label: "Users", activeButton: false }
+    ];
+
     return (
         <div className="app">
           <div className="buttons">
-            {this.state.buttons.map((button, index) => {
+            {buttons.map((button, index) => {
               return (
-                  <Button
-                      key={index}
-                      label={button.label}
-                      getContent={this.getContent}
-                  />
+                <Button
+                   key={index}
+                   label={button.label}
+                   getContent={this.getContent}
+                />
               );
             })}
           </div>
